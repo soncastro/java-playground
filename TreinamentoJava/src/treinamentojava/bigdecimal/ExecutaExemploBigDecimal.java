31cc8c11ef08039eecf51a3d9d1d4513bcbff7f1
Anderson Gomes <anderson.gomes.c@gmail.com>
2016-07-24 16:52:50 -0300

    BigDecimal

---
+++ TreinamentoJava/src/treinamentojava/bigdecimal/ExecutaExemploBigDecimal.java
@@ -0,0 +1,36 @@
+package treinamentojava.bigdecimal;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class ExecutaExemploBigDecimal {
+
+	public static void main(String[] args) {
+
+		double num1 = 0.1;
+		double num2 = 0.2;
+		double resultadoLouco1 = num1 + num2;
+	
+		System.out.print("Resultado de 0.1 + 0.2 usando double: ");
+		System.out.println(resultadoLouco1);
+		
+		BigDecimal bigDecimal1 = new BigDecimal("0.1");
+		BigDecimal bigDecimal2 = new BigDecimal("0.2");
+		BigDecimal resultadoSomaBigDecimal = bigDecimal1.add(bigDecimal2);
+		
+		System.out.print("Resultado de 0.1 + 0.2 usando BigDecimal: ");
+		System.out.println(resultadoSomaBigDecimal);
+		
+		BigDecimal bigDecimal3 = new BigDecimal("5");
+		BigDecimal bigDecimal4 = new BigDecimal("3");
+		BigDecimal resultadoDivisaoBigDecimal = bigDecimal3.divide(bigDecimal4, 2, RoundingMode.UP);
+		
+		
+		System.out.print("Resultado da divisão de 1/3 usando BigDecimal: ");
+		System.out.println(resultadoDivisaoBigDecimal);
+		
+		
+	}
+	
+}
